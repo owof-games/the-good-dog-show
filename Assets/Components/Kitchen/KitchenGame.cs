74fe6216260c6834e231dbfca92ec0ef208a3568
mattia <mattia.belletti@gmail.com>
2023-11-05 15:16:18 +0100

    chore: kitchen game refactoring

--- Assets/Components/Kitchen/KitchenGame.cs
+++ Assets/Components/Kitchen/KitchenGame.cs
@@ -1,65 +1,128 @@
-using Ink.Runtime;
-using UnityAtoms.BaseAtoms;
-using System.Linq;
-
 using UnityEngine;
 using UnityEngine.Assertions;
-using System;
-using System.Collections;
 using UnityEngine.UI;
 using DG.Tweening;
+using System.Linq;
 
 public class KitchenGame : MonoBehaviour
 {
     [SerializeField] private GameObject ingredientButtonPrefab;
-    [SerializeField] private VerticalLayoutGroup verticalLayoutGroup;
+    [SerializeField] private VerticalLayoutGroup verticalLayoutGroup1;
+    [SerializeField] private VerticalLayoutGroup verticalLayoutGroup2;
+    [SerializeField] private float recipeBookHeight = 400;
 
     private void Awake()
     {
         Assert.IsNotNull(ingredientButtonPrefab);
-        Assert.IsNotNull(verticalLayoutGroup);
+        Assert.IsNotNull(verticalLayoutGroup1);
+        Assert.IsNotNull(verticalLayoutGroup2);
     }
 
-    [SerializeField] private float outOfRecipeBookHeight = 400;
+    private RectTransform verticalLayoutRectTransform1;
+    private RectTransform verticalLayoutRectTransform2;
+    private RectTransform[] verticalLayoutRectTransforms;
 
-    private Tweener ingredientsAnimation;
+    private void Start()
+    {
+        verticalLayoutRectTransform1 = verticalLayoutGroup1.GetComponent<RectTransform>();
+        verticalLayoutRectTransform2 = verticalLayoutGroup2.GetComponent<RectTransform>();
+        verticalLayoutRectTransforms = new[]
+        {
+            verticalLayoutRectTransform1,
+            verticalLayoutRectTransform2
+        };
+    }
 
     public void PlayKitchenGame(string ingredientKeys)
     {
-        ingredientsAnimation?.Kill();
-
         ImmediatelyDestroyIngredientButtons();
 
-        foreach (var ingredientKey in ingredientKeys.Split(','))
-        {
-            var button = Instantiate(ingredientButtonPrefab, verticalLayoutGroup.transform);
-            button.GetComponent<IngredientButton>().SetupIngredient(ingredientKey);
-        }
+        CreateButtons(ingredientKeys);
 
-        // force layout to recompute
-        Canvas.ForceUpdateCanvases();
-
-        // run the ingredients button animation
-        ingredientsAnimation = verticalLayoutGroup.GetComponent<RectTransform>().DOAnchorPos(
-            new(0, outOfRecipeBookHeight + verticalLayoutGroup.preferredHeight),
-            100)
-            .SetEase(Ease.Linear)
-            .SetSpeedBased()
-            .SetLoops(-1);
+        RunIngredientButtonsAnimations();
     }
 
     private void ImmediatelyDestroyIngredientButtons()
     {
-        GameObject[] allChildren = new GameObject[verticalLayoutGroup.transform.childCount];
-        var i = 0;
-        foreach (Transform child in verticalLayoutGroup.transform)
+        foreach (var root in verticalLayoutRectTransforms)
         {
-            allChildren[i] = child.gameObject;
-            i += 1;
+            GameObject[] allChildren = new GameObject[root.childCount];
+            var i = 0;
+            foreach (Transform child in root)
+            {
+                allChildren[i++] = child.gameObject;
+            }
+            foreach (var child in allChildren)
+            {
+                DestroyImmediate(child);
+            }
         }
-        foreach (GameObject child in allChildren)
+    }
+
+    /// <summary>
+    /// Create the buttons in both vertical layout groups.
+    /// </summary>
+    /// <param name="ingredientKeys">Comma-separated list of ingredients</param>
+    private void CreateButtons(string ingredientKeys)
+    {
+        foreach (var (ingredientKey, button) in
+            from root in verticalLayoutRectTransforms
+            from ingredientKey in ingredientKeys.Split(',')
+            let button = Instantiate(ingredientButtonPrefab, root)
+            select (ingredientKey, button))
         {
-            DestroyImmediate(child);
+            button.GetComponent<IngredientButton>().SetupIngredient(ingredientKey);
         }
+    }
+
+    private Tweener ingredientsAnimation1;
+    private Tweener ingredientsAnimation2;
+
+    private void RunIngredientButtonsAnimations()
+    {
+        // kill currently running animations, if needed
+        ingredientsAnimation1?.Kill();
+        ingredientsAnimation2?.Kill();
+
+        // force layouts to recompute
+        Canvas.ForceUpdateCanvases();
+
+        // extract measures from layout groups
+        Assert.AreEqual(verticalLayoutGroup1.spacing, verticalLayoutGroup2.spacing);
+        Assert.AreEqual(verticalLayoutGroup1.preferredHeight, verticalLayoutGroup2.preferredHeight);
+        var spacing = verticalLayoutGroup1.spacing;
+        float preferredHeight = verticalLayoutGroup1.preferredHeight;
+
+        void CreateIngredientsAnimation(RectTransform myTransform, RectTransform otherTransform, System.Func<Tweener> getTweener, System.Action<Tweener> setTweener, Vector2? startPosition = null)
+        {
+            // kill the current tweener, if necessary
+            getTweener()?.Kill();
+            // start at given position, or just under the other layout
+            myTransform.anchoredPosition = startPosition ??
+                otherTransform.anchoredPosition - new Vector2(0, preferredHeight + spacing);
+            // start the animation up until it disappears
+            var tweener = myTransform
+                .DOAnchorPos(new Vector2(0, recipeBookHeight + preferredHeight), 100)
+                .SetEase(Ease.Linear)
+                .SetSpeedBased()
+                .OnComplete(() =>
+                {
+                    // restart animation just under the other layout, with automatic computation of start position
+                    CreateIngredientsAnimation(myTransform, otherTransform, getTweener, setTweener);
+                });
+            // save the new tweener
+            setTweener(tweener);
+        }
+        CreateIngredientsAnimation(
+            verticalLayoutRectTransform1,
+            verticalLayoutRectTransform2,
+            () => ingredientsAnimation1,
+            t => ingredientsAnimation1 = t,
+            Vector2.zero);
+        CreateIngredientsAnimation(
+            verticalLayoutRectTransform2,
+            verticalLayoutRectTransform1,
+            () => ingredientsAnimation2,
+            t => ingredientsAnimation2 = t);
     }
 }
