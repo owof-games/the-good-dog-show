ff1c953fd6927b4cb173fb5b112f36f680b5f0b4
Mattia Belletti <mbelletti@greenteam.it>
2024-04-07 18:11:18 +0200

    feat: localized enter input hint.

--- Assets/Components/InputHint/InputHint.cs
+++ Assets/Components/InputHint/InputHint.cs
@@ -1,6 +1,5 @@
 using UnityEngine;
 using UnityEngine.Assertions;
-using UnityEngine.UI;
 
 /// <summary>
 /// An icon showing an input hint that changes according to the current control scheme.
@@ -28,38 +27,36 @@ public class InputHint : MonoBehaviour
     [SerializeField] private Icon XboxIcon;
     [SerializeField] private Icon GamepadIcon;
 
-    [SerializeField] private Image image;
-    [SerializeField] private Sprite aSprite;
-    [SerializeField] private Sprite bSprite;
-    [SerializeField] private Sprite xSprite;
-    [SerializeField] private Sprite ySprite;
-    [SerializeField] private Sprite nSprite;
-    [SerializeField] private Sprite sSprite;
-    [SerializeField] private Sprite eSprite;
-    [SerializeField] private Sprite wSprite;
-    [SerializeField] private Sprite oneSprite;
-    [SerializeField] private Sprite twoSprite;
-    [SerializeField] private Sprite threeSprite;
-    [SerializeField] private Sprite returnSprite;
+    [SerializeField] private GameObject bImage;
+    [SerializeField] private GameObject aImage;
+    [SerializeField] private GameObject xImage;
+    [SerializeField] private GameObject yImage;
+    [SerializeField] private GameObject nImage;
+    [SerializeField] private GameObject sImage;
+    [SerializeField] private GameObject eImage;
+    [SerializeField] private GameObject wImage;
+    [SerializeField] private GameObject oneImage;
+    [SerializeField] private GameObject twoImage;
+    [SerializeField] private GameObject threeImage;
+    [SerializeField] private GameObject returnImage;
 
     private void Awake()
     {
         Assert.AreNotEqual(MouseAndKeyboardIcon, Icon.None);
         Assert.AreNotEqual(XboxIcon, Icon.None);
         Assert.AreNotEqual(GamepadIcon, Icon.None);
-        Assert.IsNotNull(image);
-        Assert.IsNotNull(aSprite);
-        Assert.IsNotNull(bSprite);
-        Assert.IsNotNull(xSprite);
-        Assert.IsNotNull(ySprite);
-        Assert.IsNotNull(nSprite);
-        Assert.IsNotNull(sSprite);
-        Assert.IsNotNull(wSprite);
-        Assert.IsNotNull(eSprite);
-        Assert.IsNotNull(oneSprite);
-        Assert.IsNotNull(twoSprite);
-        Assert.IsNotNull(threeSprite);
-        Assert.IsNotNull(returnSprite);
+        Assert.IsNotNull(aImage);
+        Assert.IsNotNull(bImage);
+        Assert.IsNotNull(xImage);
+        Assert.IsNotNull(yImage);
+        Assert.IsNotNull(nImage);
+        Assert.IsNotNull(sImage);
+        Assert.IsNotNull(wImage);
+        Assert.IsNotNull(eImage);
+        Assert.IsNotNull(oneImage);
+        Assert.IsNotNull(twoImage);
+        Assert.IsNotNull(threeImage);
+        Assert.IsNotNull(returnImage);
     }
 
     public void OnControlTypeChanged(ControlType controlType)
@@ -71,23 +68,17 @@ public class InputHint : MonoBehaviour
             ControlType.Xbox => XboxIcon,
             _ => throw new System.Exception("unknown control type")
         };
-        var sprite = icon switch
-        {
-            Icon.None => null,
-            Icon.A => aSprite,
-            Icon.B => bSprite,
-            Icon.X => xSprite,
-            Icon.Y => ySprite,
-            Icon.South => sSprite,
-            Icon.West => wSprite,
-            Icon.East => eSprite,
-            Icon.North => nSprite,
-            Icon.One => oneSprite,
-            Icon.Two => twoSprite,
-            Icon.Three => threeSprite,
-            Icon.Return => returnSprite,
-            _ => throw new System.Exception("unknown icon")
-        };
-        image.sprite = sprite;
+        bImage.SetActive(icon == Icon.B);
+        aImage.SetActive(icon == Icon.A);
+        xImage.SetActive(icon == Icon.X);
+        yImage.SetActive(icon == Icon.Y);
+        nImage.SetActive(icon == Icon.North);
+        sImage.SetActive(icon == Icon.South);
+        eImage.SetActive(icon == Icon.East);
+        wImage.SetActive(icon == Icon.West);
+        oneImage.SetActive(icon == Icon.One);
+        twoImage.SetActive(icon == Icon.Two);
+        threeImage.SetActive(icon == Icon.Three);
+        returnImage.SetActive(icon == Icon.Return);
     }
 }
