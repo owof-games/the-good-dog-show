acfedab732b9683aee5ad483a126f517c3917b88
Mattia Belletti <mbelletti@greenteam.it>
2024-04-19 21:11:34 +0200

    fix: don't skip part of choice if the text has a ':' in it

--- Assets/Components/Choices/ChoiceButton.cs
+++ Assets/Components/Choices/ChoiceButton.cs
@@ -103,7 +103,7 @@ public class ChoiceButton : MonoBehaviour
         {
             // actual choice
             var choice = storyStep.Value.Choices[myIndex];
-            var text = choice.Text.Split(':')[^1].Trim();
+            var text = choice.Text.Split(':', 2)[^1].Trim();
             textMeshPro.text = text;
 
             textMeshPro.enabled = true;
